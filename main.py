b4fce0ad99ab7782fa1778b00efb0adab7705d12
Eduardo <eduardo.mfoliveira05@gmail.com>
2026-05-22 15:57:09 -0300

    style: Introdução no console.

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Bem vindo a Calculadora!");
+print();
+
+print("Escreva o calculo com esses sinais, 1- Soma: +, 2- Subtração: -, 3- Multiplicação: *, " \
+"4- Divisão: / e 5- Porcentagem: %");
+print ("---Para %, escreve primeiro o valor que será a porcentagem.");
+print();
+
+print("Atenção!");
+print();
+
+print("Separe os valores do sinal com espaço, EX: 10 + 20 ou 10 % 100.");
+
+
